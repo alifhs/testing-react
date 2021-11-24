61a6169910b886a79dfc5b6d608173fdd7d267c3
alifhs <alif.hs@outlook.com>
2021-11-24 11:49:31 +0600

    created counter components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { Counter } from './Counter';
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-
-   function  onDecrement() {
-        if(counter > 0) {
-          setCounter(counter - 1);
-        }
-   }
+ 
   return (
     <div className="App">
-
-        <h1>This is counter app</h1>
-        <div id="counter-value">{counter}</div>
-        <button id="increment-btn" onClick={()=> {setCounter(counter + 1)}}>Increment</button>
-        <button id="decrement-btn" onClick={onDecrement}>Decrement</button>
+  
+    <Counter />
+       
     </div>
   );
 }

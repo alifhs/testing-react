0511624e29a1bad29755ee21ebda8d8e7561a275
alifhs <alif.hs@outlook.com>
2021-11-24 11:37:56 +0600

    fixed onDecrement logics

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import { useState } from 'react';
 function App() {
 
   const [counter, setCounter] = useState(0);
+
+   function  onDecrement() {
+        if(counter > 0) {
+          setCounter(counter - 1);
+        }
+   }
   return (
     <div className="App">
 
         <h1>This is counter app</h1>
         <div id="counter-value">{counter}</div>
-        <button id="increment-btn">Increment</button>
+        <button id="increment-btn" onClick={()=> {setCounter(counter + 1)}}>Increment</button>
+        <button id="decrement-btn" onClick={onDecrement}>Decrement</button>
     </div>
   );
 }
